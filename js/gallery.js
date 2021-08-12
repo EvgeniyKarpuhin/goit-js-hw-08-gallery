b1048c0e92084e76642de6a060fb8fda4dc488cf
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-13 00:24:36 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -5,7 +5,7 @@ export default class Gallery {
   _newElement;
   _lightboxImg;
   _images;
-}
+
 
 constructor(galleryItems) {
   this.makeGallery(galleryItems);
@@ -34,4 +34,73 @@ makeGallery(array) {
 
   this._ulClass = document.querySelector('ul.js-gallery');
   this._ulClass.append(...this.elementArray);
+}
+
+closeBtn() {
+  this._closeModal = document.querySelector('button[data-action="close-lightbox"]');
+  this._newElement = document.querySelector('div.lightbox__element');
+  this._lightboxImg = document.querySelector('img.lightbox__img');
+  this._images = [...document.querySelectorAll('img.gallery__image')];
+}
+
+bindImg() {
+  this._ulClass.addEventListener('click', this.onClick.bind(this));
+  this._closeModal.addEventListener('click', this.closeModal.bind(this));
+  this._newElement.addEventListener('click', this.elemClick.bind(this));
+}
+
+onClick(e) {
+  if (e.target.nodeName !== 'IMG')
+  return;
+  e.preventDefault();
+
+  this._modalImg = e.target;
+  this.openModal();
+  this.showImage(e.target.dataset.source, e.target.alt);
+}
+
+openModal() {
+  window.addEventListener('keydown', this.onPress.bind(this));
+  document.querySelector('lightbox').classList.add('is-open');
+}
+
+showImage(source, alt) {
+  this._lightboxImg.src = source;
+  this._lightboxImg.alt = alt;
+}
+
+closeModal() {
+  window.removeEventListener('keydown', this.onPress);
+  document.querySelector('.lightbox').classList.remove('is-open');
+  this._lightboxImg.src = '';
+  this._lightboxImg.alt = '';
+}
+
+elemClick(e) {
+  if(e.currentTarget === e.target)
+  this.closeModal();
+}
+
+onPress(e) {
+  if(e.code === 'Escape') this.closeModal();
+  if(e.code === 'ArrowRight') this.nextImg();
+  if(e.code === 'ArrowRLeft') this.previousImg();
+}
+
+nextImg() {
+  let nexus = this._images.indexOf(this._modalImg);
+  nexus++;
+  if(nexus === this._images.length) nexus = 0;
+  this._modalImg = this._images[nexus];
+  this.showImage(this._modalImg.dataset.source, this._modalImg.alt);
+}
+
+previousImg() {
+  let nexus = this._images.indexOf(this._modalImg);
+  nexus--;
+  if(nexus < 0)
+  nexus = this._images.length -1;
+  this._modalImg = this._images[nexus];
+  this.showImage(this._modalImg.dataset.source, this._modalImg.alt);
+}
 }
