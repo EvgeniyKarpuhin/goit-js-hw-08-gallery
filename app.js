4d2c14b4912ad0f668e23268bfbf8264754584fe
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-12 18:35:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -64,41 +64,92 @@ const galleryItems = [
     },
   ];
 
-  const images = document.querySelector('ul.js-gallery');
-  makeImages(galleryItems);
+//   const images = document.querySelector('ul.js-gallery');
+//   makeImages(galleryItems);
 
-  function makeImages(newImage) {
-    const array = newImage.map(item => {
-      return
-      `<li class = 'gallery__item'>
-      <img class = 'gallery__image'
-      src = '${item.original}'
-      alt = '${item.description}'
-      width = '320px'
+//   function makeImages(newImage) {
+//     const array = newImage.map(item => {
+//       return
+//       `<li class = 'gallery__item'>
+//       <img class = 'gallery__image'
+//       src = '${item.original}'
+//       alt = '${item.description}'
+//       width = '320px'
+//       >
+//       </li>`
+//     }).join('');
+//     images.innerHTML = array;
+//   }
+
+//   const newImage = document.querySelectorAll('img.gallery__image');
+
+//   const options = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: 1.0,
+//   }
+
+//   const img = (opened, obsrver) => {
+//     e.forEach(open => {
+//       if(open.isInterecting) {
+//         open.target.src = open.target.dataset.src;
+//         open.target.classList.add('show')
+//       }
+//     });
+//   }
+
+// const observ = new IntersectionObserver(img, options);
+// newImage.forEach(item => {
+// observ.observe(item);
+// });
+
+const containerLazyImages = document.querySelector('ul.feed');
+makeMarkupForLazyImages(galleryItems);
+
+function makeMarkupForLazyImages(imagesArray) {
+  const markup = imagesArray.map(item => {
+    return `
+    <li class="feed_item">
+      <img
+        class='lazyload'            
+        data-src='${item.original}'
+        alt='${item.description}'
+        width='320px'
+        height='240px'
       >
-      </li>`
-    }).join('');
-    images.innerHTML = array;
-  }
+      
+    </li>
+    `
+  }).join('');
 
-  const newImage = document.querySelectorAll('img.gallery__image');
+  containerLazyImages.innerHTML = markup;
+}
 
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0,
-  }
 
-  const img = (opened, obsrver) => {
-    e.forEach(open => {
-      if(open.isInterecting) {
-        open.target.src = open.target.dataset.src;
-        open.target.classList.add('show')
-      }
-    });
-  }
 
-const observ = new IntersectionObserver(img, options);
-newImage.forEach(item => {
-observ.observe(item);
+const imagesArray = document.querySelectorAll('img.lazyload');
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1.0,
+}
+
+const observFunc = (entries, observer) => {
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+      console.log('intersecting');
+      console.log(entry.target);
+        entry.target.src = entry.target.dataset.src;
+        entry.target.classList.add('appear');
+      // observer.disconnect();
+    }
+  });
+}
+
+const observer = new IntersectionObserver(observFunc, options);
+
+imagesArray.forEach(item => {
+  observer.observe(item);
 });
